6a8faf50be7aaf249664937e6c69d35c74fa556f
WilliamSteinbrink <william.steinbrink@gmail.com>
2022-01-23 08:46:17 -0500

    centered navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                         <Link className="nav-link" to="/login/">Login</Link>
                     </li>
